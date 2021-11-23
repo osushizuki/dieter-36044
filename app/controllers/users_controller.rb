1d158d5e1155d220b0bba87dd466a07ad43ae2e3
osushizuki <imaokanorihiko@icloud.com>
2021-11-23 20:04:59 +0900

    プロフィール変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email)
+    params.require(:user).permit(:nick_name, :email, :weight, :height, :age, :habit_id)
   end
 
 end
