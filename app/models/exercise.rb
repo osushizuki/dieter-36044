5c45c2f00461f2867580b454c5b9af5363005950
osushizuki <imaokanorihiko@icloud.com>
2021-11-23 11:09:32 +0900

    バリデーション

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,8 +1,8 @@
 class Exercise < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   with_options presence: true do
-    validates :menu_id
-    validates :time
+    validates :menu_id, numericality: { other_than: 1 , message: "can't be blank"}
+    validates :time, format: {with: /\A[0-9]+\z/}
   end
 
   belongs_to :user
--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,6 +1,6 @@
 class Meal < ApplicationRecord
   with_options presence: true do
-    validates :calorie
+    validates :calorie, format:{with: /\A[0-9]+\z/}
     validates :explanation
   end
 
